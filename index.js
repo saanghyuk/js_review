319f544788315b48b354325570305a086a53dc20
saanghyuk <saanghyuk@gmail.com>
2022-08-04 00:40:26 +0900

    1

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("Hello Codeit!");
+console.log(10 + 5);
+console.log(20 - 5);
+
+// 변수 선언
+let espressoPrice;
+espressoPrice = 3000;
+let espressoPrice2 = 4300;
+
+
+// 함수 선언
+// function 함수이름(){
+//   명령; 
+//   명령;
+// };
+
+function greetings(sentence){
+  console.log("Hi!");
+  console.log("안녕");
+  console.log(sentence)
+}
+greetings("Hola")
+
+function printSum(a, b){
+  console.log(a+b)
+}; 
+printSum(3+5)
